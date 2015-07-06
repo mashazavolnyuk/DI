e2a84562edc0f5f033e15fb5205a844145e88943
mashazavolnyuk <mashazavolnyuk@yandex.ru>
2015-07-06 18:32:47 +0300

    added class Connecter and change IoD

---
+++ DI/Connecter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI
+{
+    class Connecter : IConnecter
+    {
+        private string url;
+        public Connecter(string Url)
+        {
+            this.url = Url;
+        }
+
+        public bool Ping()
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                request.Timeout = 3000;
+                request.AllowAutoRedirect = false; // find out if this site is up and don't follow a redirector
+                request.Method = "HEAD";
+                using (var response = request.GetResponse())
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
--- DI/Program.cs
+++ DI/Program.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DI
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            IConnecter c = new Connecter("http://dou.ua/");
+            Console.WriteLine("Result {0}",Access(c));
+            Console.ReadLine();
+                
         }
+
+        static object Access(IConnecter Ic)
+        {
+            var c = Ic.Ping();
+            return c;
+        }
+
     }
+   
 }
+
